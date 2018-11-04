9db38b0dfc1fb6ad8c802a52b25b36eeb51301fd
sai19 <saip.koturwar@gmail.com>
2018-11-04 16:57:40 +0900

    minor fixes

--- harr.py
+++ harr.py
@@ -1,14 +1,16 @@
 import numpy as np
 import cv2
 import os
+import sys 
+import argparse 
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 def crop_face(img_path,face_cascade):
 	img = cv2.imread(img_path)
-	gray = cv2.imread(img,cv2.COLOR_BGR2GRAY)
+	gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 	faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-	max_area = 0:
+	max_area = 0
 	for (x,y,w,h) in faces:
 		if x*h>max_area:
 			max_area = x*h
@@ -16,6 +18,7 @@ def crop_face(img_path,face_cascade):
 	cropped_face = img[X:X+W,Y:Y+H]
 	cropped_face = cv2.resize(cropped_face,(160,160))
 	return cropped_face			
+
 def main(args):
 	root_dir = args.root_dir
 	files = os.listdir(root_dir)
@@ -23,14 +26,14 @@ def main(args):
 	if not os.path.exists(dest_dir):
 		os.mkdir(dest_dir)
 	for person in files:
-		person_image_path = root_dir + person
-		person_image_path_out = dest_dir+person 
+		person_image_path = os.path.join(root_dir,person)
+		person_image_path_out = os.path.join(dest_dir,person) 
 		if not os.path.exists(person_image_path_out):
 			os.mkdir(person_image_path_out)
 		for person_img in os.listdir(person_image_path):
 			cropped_face = crop_face(person_image_path+person_img)
-			out_name = person_img.split(".")[0] + _"cropped_" + person_img.split(".")[-1]
-			cv2.imwrite(person_image_path_out+out_name,cropped_face)
+			out_name = person_img.split(".")[0] + "_cropped_" + person_img.split(".")[-1]
+			cv2.imwrite(os.path.join(person_image_path_out,out_name),cropped_face)
 			
 
 
